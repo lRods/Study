538b087793f38d5cd9b8b32f4f7f8918a6037ad3
lRods <llrodgs@gmail.com>
2019-10-23 15:14:47 -0300

    Inicio curso Web

--- Python/Django/env/first_project/core/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-
-def home(request):
-        context = {'texto': 'Seu primeiro projeto Django no Linux/Ubuntu com Sublime Text 3'}
-        return render(request, 'index.html', context)
