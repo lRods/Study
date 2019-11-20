f208d4856771635ffbd3c02d44d6f233ceb835db
lRods <llrodgs@gmail.com>
2019-11-20 16:55:35 -0300

    DES APS scripts finalizados

--- Python/Faculdade/APS/DES_encrypt.py
+++ Python/Faculdade/APS/DES_encrypt.py
@@ -6,7 +6,10 @@ from Crypto.Cipher import DES
 def main():
     os.system('cls')
     print("\n ----- Criptografia DES -----\n")
+    print(" ENCRIPTAR\n")
+    encrypt()
 
+    print(" DECRIPTAR\n")
     decrypt()
 
     print("\n ----- FIM ----- \n")
@@ -17,9 +20,11 @@ def encrypt():
     # Pegando chave para encriptacao que deve ter 8 bytes
     key = input(" Insira uma senha de 8 caracteres: ")
     while True:
+        # validacao da chave com acoes caso seja maior ou menor que 8 caracteres
         if len(key) < 8 or len(key) > 8:
             os.system('cls')
             print("\n ----- Criptografia DES -----\n")
+            print(" ENCRIPTAR\n")
 
             print(" *** A senha deve ter 8 caracteres!")
             key = input(" Insira novamente: ")
@@ -28,7 +33,7 @@ def encrypt():
             break
 
     # Pegando a mensagem que sera encriptada
-    plaintext = input(" Insira o texto a ser encriptado: \n ")
+    plaintext = input("\n Insira o texto a ser encriptado: \n ")
 
     # Passando chave e mensagem para bytes para fazer a encriptacao
     keyBytes = key.encode()
@@ -60,56 +65,68 @@ def encrypt():
     plaintxtFile.write(plaintext)
     plaintxtFile.close()
 
-    print(" Mensagem encriptada: \n", msg)
+    print("\n Mensagem encriptada: \n", msg)
+    print("\n Mensagem criptografada com sucesso.\n\n")
 
 
 def decrypt():
-    # Pegando chave para decriptacao que deve ter 8 bytes
-    key = input(" Insira uma senha de 8 caracteres: ")
+    # Este loop eh para a validacao da chave, se bate com a chave salva no arquivo
     while True:
-        if len(key) < 8 or len(key) > 8:
+        # Pegando chave para decriptacao que deve ter 8 bytes
+        key = input(" Insira a senha de 8 caracteres: ")
+        while True:
+            # validacao da chave com acoes caso seja maior ou menor que 8 caracteres
+            if len(key) < 8 or len(key) > 8:
+                os.system('cls')
+                print("\n ----- Criptografia DES -----\n")
+                print(" DECRIPTAR\n")
+
+                print(" *** A senha deve ter 8 caracteres!")
+                key = input(" Insira novamente: ")
+                continue
+            else:
+                break
+
+        # Comparando a chave digitada com a chave armazenada no arquivo keyFile
+        keyFile = open('data/keyFile.txt', 'r')
+        correctKey = keyFile.read()
+        keyFile.close()
+
+        if key == correctKey:
+            # Passando a chave para bytes para a decriptacao
+            keyBytes = key.encode()
+
+            # pegando o result do arquivo msgJsonFile
+            msgJsonFile = open('data/msgJsonFile.txt', 'r')
+            result = msgJsonFile.read()
+            msgJsonFile.close()
+
+            # Separando IV e mensagem encriptada do JSON e decodificando o base64 para bytes
+            b64 = json.loads(result)
+            iv = b64decode(b64['iv'])
+            msg = b64decode(b64['msg'])
+
+            # Fazendo a decriptacao
+            cipher = DES.new(keyBytes, DES.MODE_OFB, iv=iv)
+            plaintext = cipher.decrypt(msg)
+            plaintext = plaintext.decode('utf-8')
+
+            # Comparando mensagem decriptada com a mensagem no arquivo plaintxtFile
+            plaintxtFile = open('data/plaintxtFile.txt', 'r')
+            correctPlaintext = plaintxtFile.read()
+            plaintxtFile.close()
+
+            if plaintext == correctPlaintext:
+                print(" Texto decriptado: \n", plaintext)
+                print("\n Decriptacao e validacao realizados com sucesso.")
+            else:
+                print(" Falha da decriptacao.")
+
+            break
+        else:
             os.system('cls')
             print("\n ----- Criptografia DES -----\n")
-
-            print(" *** A senha deve ter 8 caracteres!")
-            key = input(" Insira novamente: ")
-            continue
-        else:
-            break
-
-    # Comparando a chave com a chave armazenada no arquivo keyFile
-    keyFile = open('data/keyFile.txt', 'r')
-    correctKey = keyFile.read()
-    keyFile.close()
-    if key == correctKey:
-        keyBytes = key.encode()
-
-        msgJsonFile = open('data/msgJsonFile.txt', 'r')
-        result = msgJsonFile.read()
-        msgJsonFile.close()
-
-        b64 = json.loads(result)
-        iv = b64decode(b64['iv'])
-        msg = b64decode(b64['msg'])
-
-        cipher = DES.new(keyBytes, DES.MODE_OFB, iv=iv)
-        plaintext = cipher.decrypt(msg)
-
-        # Comparando mensagem decriptada com a mensagem no arquivo plaintxtFile
-        plaintxtFile = open('data/plaintxtFile.txt', 'r')
-        correctPlaintext = plaintxtFile.read()
-        plaintxtFile.close()
-
-        print(plaintext)
-
-        if plaintext == correctPlaintext:
-            print(" Texto decriptado: \n ", plaintext)
-            print("\n Decriptacao e validacao realizadas com sucesso.")
-        else:
-            print(" Falha da decriptacao.")
-    else:
-        print(" Chave incorreta. Falha na decriptacao.")
-
-
+            print("DECRIPTAR\n")
+            print(" Chave incorreta, falha na decriptacao. Tente novamente:\n")
 
 main()
